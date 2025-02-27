da1ef2715da7a786f59a75da0bbbb405790c174f
kyshu11027 <shuster5494@gmail.com>
2025-02-26 22:38:15 -0500

    add cors to s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -17,3 +17,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "session_images_lifecycle" {
     }
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "session_images_cors" {
+  bucket = aws_s3_bucket.session_images.id
+
+  cors_rule {
+    allowed_origins = terraform.workspace == "dev" ? ["https://pickpix.vercel.app", "http://localhost:3000"] : ["https://pickpix.vercel.app"]
+    
+    allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
+    allowed_headers = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
