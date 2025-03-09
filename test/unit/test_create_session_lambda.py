9a6b63df7c5537abdc30ca073762fed19672726d
kyshu11027 <shuster5494@gmail.com>
2025-03-08 19:34:03 -0500

    securing DB secrets in test env

--- test/unit/test_create_session_lambda.py
+++ test/unit/test_create_session_lambda.py
@@ -3,6 +3,7 @@ import os
 import moto
 from boto3 import resource, client
 from unittest import TestCase
+from dotenv import load_dotenv
 import psycopg2
 import json
 
@@ -24,23 +25,20 @@ class TestCreateSession(TestCase):
         Create our mocked cloud resources
         """
         # Test variables
+        load_dotenv()
         self.utils = UnitTestUtils()
         self.region = 'us-east-1'
         self.test_s3_bucket_name = "unit_test_photo_ranker_s3_bucket"
         self.test_access_key = "unit_test_access_key"
         self.test_secret_key = "unit_test_secret_key"
-        self.db_host = 'photo-ranker-db-dev.c5yogmykmh2v.us-east-2.rds.amazonaws.com'
-        self.db_name = 'photoranker'
-        self.db_user = 'photorankeradmin'
-        self.db_password = 'photoR4nk3r'
+        self.db_host = os.environ["DB_HOST"]
+        self.db_name = os.environ['DB_NAME']
+        self.db_user = os.environ['DB_USER']
+        self.db_password = os.environ['DB_PASSWORD']
         # Set environment variables
         os.environ["S3_BUCKET_NAME"] = self.test_s3_bucket_name
         os.environ["AWS_ACCESS_KEY_ID"] = self.test_access_key
         os.environ["AWS_SECRET_ACCESS_KEY"] = self.test_secret_key
-        os.environ["DB_HOST"] = self.db_host
-        os.environ['DB_NAME'] = self.db_name
-        os.environ['DB_USER'] = self.db_user
-        os.environ['DB_PASSWORD'] = self.db_password
 
         # Construct s3 mock service
         self.s3_resource = resource('s3', self.region)
@@ -75,7 +73,7 @@ class TestCreateSession(TestCase):
             self.assertTrue(str(session_url) in id)
             self.assertTrue("https://" in url)
 
-        # Verify item was actually created in DynamoDB
+        # Verify item was actually created in database
         try:
             connection = psycopg2.connect(
                 host=self.db_host,
@@ -84,22 +82,33 @@ class TestCreateSession(TestCase):
                 password=self.db_password
             )
             cursor = connection.cursor()
+        except Exception as e:
+            self.fail(f'Failed to connect to database: {str(e)}')
+    
+        try:
             # Assert that the rows were written in the sessions table
             cursor.execute("SELECT * FROM sessions WHERE session_id = %s", (session_id,))
             rows = cursor.fetchall()
             self.assertTrue(len(rows) == 1)
-
+        
+        except Exception as e:
+            self.fail(f"Failed to retrieve session from Postgres: {str(e)}")
+        
+        try:
             # Assert that the rows were written in the photos table
             cursor.execute("SELECT * FROM photos WHERE session_id = %s", (session_id,))
             photo_id = cursor.fetchone()[0]
             self.assertTrue(image_ids[0].get('id', '') == photo_id)
-
+        except Exception as e:
+            self.fail(f"Failed to retrieve photo from Postgres: {str(e)}")
+        
+        try:
             # Assert that the rows were written to the reactions table
             cursor.execute("SELECT * FROM reactions WHERE photo_id = %s", (photo_id,))
             rows = cursor.fetchall()
             self.assertTrue(len(rows) == 1)
         except Exception as e:
-            self.fail(f"Failed to retrieve item from Postgres: {str(e)}")
+            self.fail(f"Failed to retrieve reaction from Postgres: {str(e)}")
 
     def tearDown(self) -> None:
         s3_bucket = self.s3_resource.Bucket(self.test_s3_bucket_name)
