d0b6acab9f2012ef9fa5089407c2683bdd2aae54
kyshu11027 <shuster5494@gmail.com>
2025-03-20 22:50:29 -0400

    added variable to terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,7 @@ data "aws_caller_identity" "current" {}
 variable "db_user" {}
 variable "db_password" {}
 variable "auth0_domain" {}
+variable "api_audience" {}
 
 locals {
   aws_account_id = data.aws_caller_identity.current.account_id
